6eed1a9e0c00fde33d6adb8f8bef7d0949d3554d
Roy Rui <yrui053@uottawa.ca>
2024-09-26 19:46:17 -0400

    Report and test

--- Lab1/lab101/bin/main.ml
+++ Lab1/lab101/bin/main.ml
@@ -9,7 +9,7 @@ let rec map2 f l1 l2 =
 
 
 let () =
-  let result = map2 (fun x y -> x + y) [1;2;3] [4;5;6] in
+  let result = map2 (fun x y -> x + y) [1; 2; 3] [4; 5; 6] in
   Printf.printf "int list = [";
   Printf.printf "%s" (String.concat ";" (List.map string_of_int result));
   Printf.printf "]\n"
--- Lab1/lab102/bin/main.ml
+++ Lab1/lab102/bin/main.ml
@@ -6,7 +6,7 @@ let rec filter_even l1 =
       else filter_even tail
 
 let () =
-  let result = filter_even [1; 2; 3; 4; 5; 6; 7; 8] in
+  let result = filter_even [1; 2; 3; 4; 5; 6] in
   Printf.printf "The int list = [";
   Printf.printf "%s" (String.concat ";" (List.map string_of_int result));
   Printf.printf "]";
