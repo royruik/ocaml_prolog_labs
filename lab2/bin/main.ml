7f2f285717777088c23b6e04c20ba8bed95b14fa
30017134Ruoyu <rliu144@uottawa.ca>
2024-09-27 14:18:29 -0400

    step 1 and 2

--- lab2/bin/main.ml
+++ lab2/bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+type job_scheduling = {
+  priority of int
+  duration of int
+  start_time of int
+}
+
+
+let time_to_minutes hour minute = 
+  hour * 60 + minute
+
+let read_jobs 
